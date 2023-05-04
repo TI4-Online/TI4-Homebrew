85717ae53fdbdd5b2b82b2dee344d70828786f0a
Will Holton <willholton1@gmail.com>
2023-05-03 23:48:25 -0400

    Additive loading

---
+++ src/action-deck-2/inject-add.js
@@ -0,0 +1,14 @@
+const { world } = require("@tabletop-playground/api");
+const UNIT_MODIFIERS = require("./unit-modifiers.data");
+const LOCALE_STRINGS = require("./locale-strings.data");
+
+world.TI4.homebrew.inject({
+    nsidToTemplateId:
+    {
+        "card.action:homebrew.action_deck_2/0": "934D6DF9DCB340AD826E2416C7C78580",
+    },
+    unitModifiers: UNIT_MODIFIERS,
+    localeStrings: LOCALE_STRINGS
+  });
+
+world.TI4.homebrew.resetOnTableDecks()
