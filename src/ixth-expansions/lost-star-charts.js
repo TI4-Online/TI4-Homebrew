12d5a31f904fc9eabf1ed6f19c9d52049ec6f58a
Christian V <lonwyrm@gmail.com>
2023-05-06 10:49:28 +0200

    adding remaining cards and token (incl. templates)

--- src/ixth-expansions/lost-star-charts.js
+++ src/ixth-expansions/lost-star-charts.js
@@ -31,6 +31,14 @@ var nsidToTemplateId = {
         "803F329D455EA6E1323C1982528C7F84",
     "card.legendary_planet:homebrew.ixth_lost_star_charts/legendary_planets":
         "E1DAC6584B3710A9F5A1859D01F71C62",
+    "card.terraforming_initiative:homebrew.ixth_lost_star_charts/0":
+        "F0BD707740DF3A205984B3A585F5C185",
+    "card.exploration.cultural:homebrew.ixth_lost_star_charts/star_map_relic_fragment_cultural":
+        "6C9D4F134FD83323FE4397A5E656634C",
+    "card.exploration.cultural:homebrew.ixth_lost_star_charts/star_map_relic_fragment_hazardous":
+        "6DA8FD774AE357B4A584AB93C153C8BD",
+    "card.exploration.cultural:homebrew.ixth_lost_star_charts/star_map_relic_fragment_industrial":
+        "A1F7F46D4C79E98A9BEF0EABAC518775",
     "tile.system:homebrew.ixth_lost_star_charts/3401":
         "3B52D8FF421EA0EAC360BF869BF7CF1A",
     "tile.system:homebrew.ixth_lost_star_charts/3402":
@@ -63,6 +71,32 @@ var nsidToTemplateId = {
         "144B7A254C13368D06CE4DAD3DD99919",
     "tile.system:homebrew.ixth_lost_star_charts/3416":
         "41E4B1AC45F2CA2842856781D69AABA3",
+    "token.attachment:homebrew.ixth_lost_star_charts/cultural_boom":
+        "E0C728D640471316384D3392EEEC17B0",
+    "token.attachment:homebrew.ixth_lost_star_charts/established_trade_route": 
+        "ED12EBF14E6A95F9BCDC22BADE81F6D1",
+    "token.attachment:homebrew.ixth_lost_star_charts/galatic_financial_center":
+        "0A042A6E46E8C8010DAC72B304890B61",
+    "token.attachment:homebrew.ixth_lost_star_charts/geothermal_equilibirum":
+        "4680B47E49F02C8A3CCD318F395A230A",
+    "token.attachment:homebrew.ixth_lost_star_charts/industrial_boom":
+        "083BB63B485DABF6A8345D967F7CA3DD",
+    "token.attachment:homebrew.ixth_lost_star_charts/interplanetary_hub":
+        "A7E35EAA414203346D515EAB423704E3",
+    "token.attachment:homebrew.ixth_lost_star_charts/orbital_slingshot":
+        "A14476FD492B395C0A12A09E45D5B94A",
+    "token.attachment:homebrew.ixth_lost_star_charts/planetary_archologies":
+        "BB273BE846D50F7285F1F6A8F45E949F",
+    "token.attachment:homebrew.ixth_lost_star_charts/population_growth":
+        "A64EE79F40D722314552669A5BBDFD95",
+    "token.attachment:homebrew.ixth_lost_star_charts/self-defense_initiative":
+        "FD832139487457AC8AAC2A9E9DA1DBA5",
+    "token.attachment:homebrew.ixth_lost_star_charts/terraforming_milestone":
+        "32DBB1A4422E5BA38C28A79C783BE3EB",
+    "token.commodity:homebrew.ixth_lost_star_charts/lengendary_planet_jarrah":
+        "80688203469E96E074957488799C227B",
+    "token.commodity:homebrew.ixth_lost_star_charts/lengendary_planet_kwon":
+        "2D822E6C49C394F11E9815AF1F4A7E5E",
 };
 
 // System IDs are set in the 4k range and are using their printed ID: i.e. Alpert (14F) is 4014
@@ -325,5 +359,7 @@ world.TI4.homebrew.inject(
     systems,
 );
 
+// rebuild decks with injected cards
+world.TI4.homebrew.resetOnTableDecks();
 
 console.log("HOMEBREW ADDING LOST STAR CHARTS");
