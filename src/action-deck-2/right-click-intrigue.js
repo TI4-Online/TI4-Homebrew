a63c528a9d7f647be49cc8914de4b4915fb574ff
Will Holton <willholton1@gmail.com>
2023-05-10 10:40:37 -0400

    Intrigue right click script

---
+++ src/action-deck-2/right-click-intrigue.js
@@ -0,0 +1,37 @@
+const ACTION_NAME = "*Draw bottom 2 agendas";
+const AGENDA_COUNT = "2";
+
+/**
+ *    Intrigue
+ *    After an agenda is revealed:
+ *    Look at the bottom 2 cards of the agenda deck. Place each card on the top or bottom of the deck in any order. Then, if you did not place either card on top of the deck, draw 1 action card.
+ */
+class HackElection extends world.TI4.AbstractRightClickCard {
+    
+    constructor() {
+        super();
+    }
+
+    isRightClickable(card) {
+        const nsid = world.TI4.ObjectNamespace.getNsid(card);
+        return nsid === "card.action:homebrew.action_deck_2/intrigue";
+    }
+
+    getRightClickActionNamesAndTooltips(card) {
+        return [{ actionName: ACTION_NAME, tooltip: undefined }];
+    }
+    
+    onRightClick(card, player, selectedActionName) {
+        if (selectedActionName !== ACTION_NAME) {
+            return;
+        }
+        const deck = world.TI4.DealDiscard.getDeckWithReshuffle("card.agenda", AGENDA_COUNT);
+        const fromFront = true; // from the bottom
+        const offset = 0;
+        const keep = false; // removes from the deck
+        let agendas = deck.takeCards(AGENDA_COUNT, fromFront, offset, keep);
+        world.TI4.CardUtil.moveCardsToCardHolder(agendas, player.getSlot());
+    }
+}
+
+new HackElection();
