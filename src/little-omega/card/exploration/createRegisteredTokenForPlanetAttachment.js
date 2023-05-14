ca8c695bd16b4ac5718af1694b10884bca116b76
Will Holton <willholton1@gmail.com>
2023-05-14 17:36:18 -0400

    fix tokens

---
+++ src/little-omega/card/exploration/createRegisteredTokenForPlanetAttachment.js
@@ -0,0 +1,2 @@
+const { refObject } = require('@tabletop-playground/api');
+world.TI4.AbstractPlanetAttachment.createForKnownAttachmentToken(refObject);
--- src/little-omega/card/relic/inject-relic.js
+++ src/little-omega/card/relic/inject-relic.js
@@ -3,6 +3,22 @@ const UNIT_MODIFIERS = require("./unit-modifiers.data");
 require("./right-click-nano-forge");
 world.__littleOmegaExplorationLoaded = false;
 
+const OTHER_SCORABLE = ["card.relic:homebrew.little-omega/the_crown_of_emphidia"];
+
+const ATTACHMENTS = [
+    {
+        localeName: "token.attachment.nano_forge",
+        cardNsid: "card.relic:homebrew.little-omega/nanoforge",
+        tokenNsid: "token.attachment.exploration:pok/nano_forge",
+        faceUp: {
+            resources: 2,
+            influence: 2,
+            legendary: true,
+            image: "global/tokens/pok/exploration/exploration_2-2-legend_c.png",
+        }
+    }
+];
+
 world.TI4.homebrew.inject({
     nsidToTemplateId:
     {
@@ -15,7 +31,9 @@ world.TI4.homebrew.inject({
                 "card.relic:codex.affinity/nanoforge": "card.relic:homebrew.little-omega/nanoforge",
                 "card.relic:pok/the_crown_of_thalnos": "card.relic:homebrew.little-omega/the_crown_of_thalnos"
     },
-    unitModifiers: UNIT_MODIFIERS
+    unitModifiers: UNIT_MODIFIERS,
+    otherScorable: OTHER_SCORABLE,
+    attachments: ATTACHMENTS
 });
 
 if (!world.__littleOmegaFull && !world.__littleOmegaExplorationLoaded) {
