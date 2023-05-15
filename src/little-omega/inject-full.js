e0d4e8f689902e45491abe2eb3d32d8cb5d590a4
Will Holton <willholton1@gmail.com>
2023-05-15 04:35:00 -0400

    Small fix (again)

--- src/little-omega/inject-full.js
+++ src/little-omega/inject-full.js
@@ -4,8 +4,8 @@ require("./card/action/inject-action");
 require("./card/agenda/inject-agenda");
 require("./card/objective/inject-objective");
 require("./card/promissory/inject-promissory");
-require("./card/promissory/inject-exploration");
-require("./card/promissory/inject-relic");
+require("./card/exploration/inject-exploration");
+require("./card/relic/inject-relic");
 
 if (!world.__littleOmegaFullLoaded) {
     world.TI4.homebrew.resetOnTableDecks();
